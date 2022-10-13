d98eade030159f7a1f3f5cac729d00d720256df8
Huu Chi Huynh <73843190+HuuChiHuynh@users.noreply.github.com>
2022-10-13 22:29:24 +0100

    Revert "Removed keep_alive.py"
    This reverts commit a5afea5561ec4850722bfa30bb93f9c250cb8909.

--- src/main.py
+++ src/main.py
@@ -11,6 +11,7 @@ from jsonschema import validate
 import discord
 from discord import app_commands
 from discord.ext import tasks
+from keep_alive import keep_alive
 
 from timetable import Timetable
 
@@ -158,6 +159,7 @@ class TimetableClient(discord.Client):
                 except ValueError:
                     pass
 
+keep_alive()  # Starts a webserver to be pinged.
 intents = discord.Intents.default()
 client = TimetableClient(intents=intents)
 
