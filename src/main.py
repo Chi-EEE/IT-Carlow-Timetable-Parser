09ffbf03f1ad4119b5415be62ee23d9f12a4b4da
Huu Chi Huynh <73843190+HuuChiHuynh@users.noreply.github.com>
2022-10-13 12:41:05 +0100

    Made TimetableClient class
    Every method is moved into the TimetableClient
    Removed .replit

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import asyncio
 from io import BytesIO
 import os
 from dotenv import load_dotenv
@@ -18,16 +19,29 @@ load_dotenv()
 
 MINUTES = 2
 
-intents = discord.Intents.default()
-client = discord.Client(intents=intents)
-tree = app_commands.CommandTree(client)
-
 timetable_name_to_id = {}
 
 # List of timetables to update
 timetables = {}
 
 
+timetable_channel_schema = {
+    "type": "object",
+    "properties": {
+        "url": {"type": "string"},
+        "id": {"type": "string"},
+        "hash": {"type": "string"},
+    },
+    "additionalProperties": False,
+}
+
+
+# This is to check if the channel was set up properly
+async def get_timetable_hash(timetable_url, timetable_id):
+    return hashlib.sha256(
+        (os.getenv("PRIVATE_HASH") + timetable_url + timetable_id).encode()
+    ).hexdigest()
+    
 async def setup_timetable_name_to_id():
     response = requests.get("http://timetable.itcarlow.ie/js/filter.js")
     if response.status_code == 200:
@@ -46,109 +60,119 @@ async def setup_timetable_name_to_id():
     else:
         print("Unable to get filter.js from timetable.itcarlow.ie")
 
-
-timetable_channel_schema = {
-    "type": "object",
-    "properties": {
-        "url": {"type": "string"},
-        "id": {"type": "string"},
-        "hash": {"type": "string"},
-    },
-    "additionalProperties": False,
-}
-
-# This is to check if the channel was set up properly
-async def get_timetable_hash(timetable_url: str, timetable_id: str):
-    return hashlib.sha256(
-        (os.getenv("PRIVATE_HASH") + timetable_url + timetable_id).encode()
-    ).hexdigest()
+class TimetableClient(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.tree = app_commands.CommandTree(self)
 
 
-async def post_timetable_screenshot(
-    channel: discord.TextChannel,
-    timetable_id: str,
-    timetable_screenshot: BytesIO,
-):
-    image_file = discord.File(timetable_screenshot, filename=f"{timetable_id}.png")
-    await channel.send(file=image_file)
+    async def on_ready(self):
+        print(f"We have logged in as {self.user}")
+        await self.tree.sync()
+        
 
-
-async def send_message(
-    channel: discord.TextChannel, message: str, syntax_language: str, file_name: str
-):
-    """
-    Sends a message whether the string is less than or equal to 2000 characters long or a text file if the string longer than 2000 characters.
-    Parameters
-    ------------
-    channel: :class:`~discord.Channel`
-        The channel to send the message to.
-    message: :class:`~str`
-        The message which to send to the channel.
-    syntax_language: :class:`~str`
-        The syntax language highlight of the message.
-    file_name: :class:`~str`
-        The file name of the message file if it is over 2000 characters.
-    """
-    if message != "":
-        if len(message) <= 2000:
-            await channel.send(
-                content=f"""```{syntax_language}
-            {message}```"""
-            )
-        else:
-            text_file = discord.File(
-                BytesIO(bytes(message, encoding="utf-8")),
-                filename=f"{file_name}.{syntax_language}",
-            )
-            await channel.send(file=text_file)
-
-
-async def send_timetable_alert(
-    channel: discord.TextChannel,
-    timetable_id: str,
-    current_timetable: str,
-    timetable_diff: str,
-    timetable_screenshot: BytesIO,
-):
-    await send_message(channel, current_timetable, "json", timetable_id)
-    await post_timetable_screenshot(channel, timetable_id, timetable_screenshot)
-    await send_message(channel, timetable_diff, "diff", "Difference")
-
-
-@tasks.loop(minutes=MINUTES)
-async def alert_timetable():
-    for timetable_id, timetable in timetables.items():
-        await timetable.clear()
-        await timetable.create_default()
-        for channel in timetable.channels:
-            timetable_diff = await timetable.get_previous_timetable_diff(channel)
-            if timetable_diff != "":
-                await send_timetable_alert(
-                    channel,
-                    timetable_id,
-                    timetable.JSON_STRING,
-                    timetable_diff,
-                    timetable.SCREENSHOT,
-                )
-
-
-async def assign_timetable(timetable_id: str, channel: discord.TextChannel):
-    if timetable_id not in timetables:
-        timetable = Timetable(client, timetable_id)
-        timetables[timetable_id] = timetable
-        await timetable.create_default()
-    timetable = timetables[timetable_id]
-    await timetable.add_channel(channel=channel)
-    timetable_diff = await timetable.get_previous_timetable_diff(channel)
-    await send_timetable_alert(
+    async def post_timetable_screenshot(
+        self, 
         channel,
         timetable_id,
-        timetable.JSON_STRING,
-        timetable_diff,
-        timetable.SCREENSHOT,
-    )
+        timetable_screenshot,
+    ):
+        image_file = discord.File(timetable_screenshot, filename=f"{timetable_id}.png")
+        await channel.send(file=image_file)
 
 
+    async def send_message(
+        self, channel, message, syntax_language, file_name
+    ):
+        """
+            Sends a message whether the string is less than or equal to 2000 characters long or a text file if the string longer than 2000 characters.
+            Parameters
+            ------------
+            channel: :class:`~discord.Channel`
+                The channel to send the message to.
+            message: :class:`~str`
+                The message which to send to the channel.
+            syntax_language: :class:`~str`
+                The syntax language highlight of the message.
+            file_name: :class:`~str`
+                The file name of the message file if it is over 2000 characters.
+                """
+        if message != "":
+            if len(message) <= 2000:
+                await channel.send(
+                    content=f"""```{syntax_language}
+                {message}```"""
+                )
+            else:
+                text_file = discord.File(
+                    BytesIO(bytes(message, encoding="utf-8")),
+                    filename=f"{file_name}.{syntax_language}",
+                )
+                await channel.send(file=text_file)
+
+
+    async def send_timetable_alert(self, channel, timetable_id, current_timetable, timetable_diff, timetable_screenshot):
+        await self.send_message(channel, current_timetable, "json", timetable_id)
+        await self.post_timetable_screenshot(
+            channel, timetable_id, timetable_screenshot
+        )
+        await self.send_message(channel, timetable_diff, "diff", "Difference")
+
+
+    @tasks.loop(minutes=MINUTES)
+    async def alert_timetable(self):
+        for timetable_id, timetable in timetables.items():
+            await timetable.clear()
+            await timetable.create_default()
+            for channel in timetable.channels:
+                timetable_diff = await timetable.get_previous_timetable_diff(channel)
+                if (timetable_diff != ""):
+                    await self.send_timetable_alert(channel, timetable_id, timetable.JSON_STRING, timetable_diff, timetable.SCREENSHOT)
+
+
+    async def assign_timetable(self, timetable_id, channel):
+        if timetable_id not in timetables:
+            timetable = Timetable(self, timetable_id)
+            timetables[timetable_id] = timetable
+            await timetable.create_default()
+        timetable = timetables[timetable_id]
+        await timetable.add_channel(channel=channel)
+        timetable_diff = await timetable.get_previous_timetable_diff(channel)
+        await self.send_timetable_alert(channel, timetable_id, timetable.JSON_STRING, timetable_diff, timetable.SCREENSHOT)
+
+
+    async def get_timetable_channels(self):
+        channels = self.get_all_channels()
+        while channel := next(channels, None):
+            if isinstance(channel, discord.TextChannel) and channel.topic:
+                try:
+                    timetable_info = json.loads(channel.topic)
+                    validate(instance=timetable_info, schema=timetable_channel_schema)
+                    timetable_id = timetable_info.get("id")
+                    timetable_url = timetable_info.get("url")
+                    real_hash = await get_timetable_hash(timetable_url, timetable_id)
+                    if real_hash == timetable_info.get("hash"):
+                        await self.assign_timetable(timetable_id, channel)
+                        break
+                except jsonschema.ValidationError:
+                    pass
+                except ValueError:
+                    pass
+
+keep_alive()  # Starts a webserver to be pinged.
+intents = discord.Intents.default()
+client = TimetableClient(intents=intents)
+
+async def handle_timetable():
+    await setup_timetable_name_to_id()
+    client.wait_until_ready()
+    await client.get_timetable_channels()
+    await client.alert_timetable.start()
+
+loop = asyncio.new_event_loop()
+ready_task = loop.create_task(handle_timetable())
+
+tree = client.tree
 @tree.command(
     name="timetable_assign",
     description="Designate the discord channel as a timetable channel",
@@ -183,7 +207,7 @@ async def timetable_assign(
             await interaction.response.send_message(
                 f"The timetable channel has been assigned.", ephemeral=True
             )
-            await assign_timetable(timetable_id, channel)
+            await client.assign_timetable(timetable_id, channel)
         else:
             await interaction.response.send_message(
                 f"The requested timetable id ({timetable_id}) is invalid.",
@@ -195,33 +219,4 @@ async def timetable_assign(
         )
 
 
-async def get_timetable_channels():
-    channels = client.get_all_channels()
-    while channel := next(channels, None):
-        if isinstance(channel, discord.TextChannel) and channel.topic:
-            try:
-                timetable_info = json.loads(channel.topic)
-                validate(instance=timetable_info, schema=timetable_channel_schema)
-                timetable_id = timetable_info.get("id")
-                timetable_url = timetable_info.get("url")
-                real_hash = await get_timetable_hash(timetable_url, timetable_id)
-                if real_hash == timetable_info.get("hash"):
-                    await assign_timetable(timetable_id, channel)
-                    break
-            except jsonschema.ValidationError:
-                pass
-            except ValueError:
-                pass
-
-
-@client.event
-async def on_ready():
-    print(f"We have logged in as {client.user}")
-    await setup_timetable_name_to_id()
-    await tree.sync()
-    await get_timetable_channels()
-    alert_timetable.start()
-
-
-keep_alive()  # Starts a webserver to be pinged.
 client.run(os.getenv("TOKEN"))
